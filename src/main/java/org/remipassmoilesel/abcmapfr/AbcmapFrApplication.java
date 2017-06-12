1bd349e8c98d13b4e019b61019b75690d4e8ce6f
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-06-12 21:59:30 +0200

    First Spring setup and template

---
+++ src/main/java/org/remipassmoilesel/abcmapfr/AbcmapFrApplication.java
@@ -0,0 +1,12 @@
+package org.remipassmoilesel.abcmapfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcmapFrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcmapFrApplication.class, args);
+	}
+}
