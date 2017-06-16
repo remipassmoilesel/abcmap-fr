6d1a872d605ced7d05801a05a460a4931f8ac4b8
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-06-16 13:04:37 +0200

    Typo and style fix on welome page

--- src/main/java/org/remipassmoilesel/abcmapfr/lists/Functionalities.java
+++ src/main/java/org/remipassmoilesel/abcmapfr/lists/Functionalities.java
@@ -11,21 +11,21 @@ public class Functionalities {
     public static List<String> getList(){
         return Arrays.asList(
 
-                "<b>Importez et assemblez automatiquement un fond de carte</b> à partir d'images ou"
-                        + "de captures d'écran. Les sources sont nombreuses: sites de cartographie en ligne,"
+                "<b>Importez et assemblez automatiquement un fond de carte</b> à partir d'images ou "
+                        + "de captures d'écran. Les sources sont nombreuses: sites de cartographie en ligne, "
                         + "cartes numérisées, images aériennes, ...",
 
-                "<b>Tracez des formes vectorielles</b>, ins&eacute;rez des symboles, des &eacute;tiquettes de texte"
+                "<b>Tracez des formes vectorielles</b>, ins&eacute;rez des symboles, des &eacute;tiquettes de texte "
                         + "et travaillez avec des calques à transparence variable.",
 
-                "<b>G&eacute;o-r&eacute;f&eacute;rencez vos cartes</b> pour utiliser un syst&egrave;me de"
-                        + "coordonn&eacute;es sans difficult&eacute;s. Indiquez seulement les coordonn&eacute;es de deux"
+                "<b>G&eacute;o-r&eacute;f&eacute;rencez vos cartes</b> pour utiliser un syst&egrave;me de "
+                        + "coordonn&eacute;es sans difficult&eacute;s. Indiquez seulement les coordonn&eacute;es de deux "
                         + "points sur la carte.",
 
-                "<b>Mesurez des distances</b> et relevez des azimuts à partir de formes géométriques simples.",
+                "<b>Mesurez des distances</b> et relevez des azimuts à partir de formes géométriques simples. ",
 
-                "<b>Mettez en forme vos cartes pour impression</b> en quelques clics. Utilisez dans un même document"
-                        + "plusieurs formats standards ou personnalis&eacute;s."
+                "<b>Mettez en forme vos cartes pour impression</b> en quelques clics. Utilisez dans un même document "
+                        + "plusieurs formats standards ou personnalis&eacute;s. "
 
         );
     }
